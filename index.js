ff69914b4b757df78cada3fac2b5ed185fb25f0b
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-18 03:08:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(Ralph) {
-  return kittens.push("Ralph")
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
 }
